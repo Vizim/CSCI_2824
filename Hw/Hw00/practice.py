75e2a031bd3381bed7d43eda8a91878ad086a332
ItzVizual <zainelsell@gmail.com>
2022-01-24 16:35:27 -0700

    added tex source files

---
+++ Hw/Hw00/practice.py
@@ -0,0 +1,12 @@
+#DO NOT CHANGE THE NAMES OF THE FILE, CLASS or METHOD
+# 
+# Zain Elsell
+# Discrete Structures
+# Homework 0 - Coding
+# 
+
+class Practice(object):
+	def add(self,a,b):
+		#Start writing your code from here
+		self = 0 #instance intialized to zero
+		return a+b#return arg2 , arg3 summed together
---
+++ Hw/Hw02/hw2.py
@@ -0,0 +1,7 @@
+#DO NOT CHANGE THE NAMES OF THE FILE, CLASS or METHOD
+
+class LogicProgramming():
+    def classDirectory(self,instructor, student, teaches):
+        for x in range(len(instructor)):
+            if(instructor[x])
+        return False
